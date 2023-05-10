6025334f729f5c8477a9c008eec122b45bfd47d7
Felix <felix.schmid@festech.de>
2023-05-10 17:34:13 +0200

    fix

--- src/main.rs
+++ src/main.rs
@@ -53,26 +53,45 @@ fn create_ifnot_exitsts(folder: String, user: &String, server: &String) {
     cmd.output().expect("failed to execute process");
 }
 
-fn create_folders(target_folder: String, user: String, server: String) {
+fn create_houry(target_folder: String, user: String, server: String) {
     create_ifnot_exitsts(format!("{}/hourly", target_folder), &user, &server);
-    create_ifnot_exitsts(format!("{}/weekly", target_folder), &user, &server);
-    create_ifnot_exitsts(format!("{}/monthly", target_folder), &user, &server);
-
     for i in 1..25 {
-        let user = user.clone();
-        let server = server.clone();
         create_ifnot_exitsts(format!("{}/hourly/{}", target_folder, i), &user, &server);
     }
-    for i in 1..6 {
-        let user = user.clone();
-        let server = server.clone();
-        create_ifnot_exitsts(format!("{}/weekly/{}", target_folder, i), &user, &server);
-    }
+}
+
+fn create_monthly(target_folder: String, user: String, server: String) {
+    create_ifnot_exitsts(format!("{}/monthly", target_folder), &user, &server);
     for i in 1..13 {
         let user = user.clone();
         let server = server.clone();
         create_ifnot_exitsts(format!("{}/monthly/{}", target_folder, i), &user, &server);
     }
+}
+
+fn create_weekly(target_folder: String, user: String, server: String) {
+    create_ifnot_exitsts(format!("{}/weekly", target_folder), &user, &server);
+    for i in 1..6 {
+        let user = user.clone();
+        let server = server.clone();
+        create_ifnot_exitsts(format!("{}/weekly/{}", target_folder, i), &user, &server);
+    }
+}
+
+fn create_daily(target_folder: String, user: String, server: String) {
+    create_ifnot_exitsts(format!("{}/daily", target_folder), &user, &server);
+    for i in 1..8 {
+        let user = user.clone();
+        let server = server.clone();
+        create_ifnot_exitsts(format!("{}/daily/{}", target_folder, i), &user, &server);
+    }
+}
+
+fn create_folders(target_folder: String, user: String, server: String) {
+    create_houry(target_folder.clone(), user.clone(), server.clone());
+    create_daily(target_folder.clone(), user.clone(), server.clone());
+    create_weekly(target_folder.clone(), user.clone(), server.clone());
+    create_monthly(target_folder.clone(), user.clone(), server.clone());
 }
 
 fn print_success(success: bool, label: String) {
